3fcac63ac8fd3aefeb3e51122ab53b88a8751a0d
ChernakovaMSPB <115102637+ChernakovaMSPB@users.noreply.github.com>
2023-04-20 00:33:22 +0300

    Add files via upload

--- src/main/java/rru/yandex/practicum/filmorate/storage/FilmDbStorage.java
+++ src/main/java/rru/yandex/practicum/filmorate/storage/FilmDbStorage.java
@@ -21,30 +21,29 @@ public class FilmDbStorage implements FilmStorage {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    @SuppressWarnings("checkstyle:LocalVariableName")
     @Override
     public Film create(Film film) {
-        String QUERY_SQL = "insert into films (name, description, release_date, duration, rating_id) " +
+        String querySql = "insert into films (name, description, release_date, duration, rating_id) " +
                 "values (?, ?, ?, ?, ?)";
         int ratingId = film.getMpa().getId();
         film.setGenres(getUniqueGenres(film.getGenres()));
 
-        jdbcTemplate.update(QUERY_SQL,
+        jdbcTemplate.update(querySql,
                 film.getName(),
                 film.getDescription(),
                 film.getReleaseDate(),
                 film.getDuration(),
                 ratingId);
 
-        String QUERY_FOR_LAST_ID_SQL = "select film_id from films order by film_id desc limit 1";
-        SqlRowSet rowSet = jdbcTemplate.queryForRowSet(QUERY_FOR_LAST_ID_SQL);
+        String queryForLastIdSql = "select film_id from films order by film_id desc limit 1";
+        SqlRowSet rowSet = jdbcTemplate.queryForRowSet(queryForLastIdSql);
         if (rowSet.next()) {
             film.setId(rowSet.getLong("film_id"));
             if (film.getGenres().size() > 0) {
-                String GENRE_QUERY_SQL = "insert into film_genre (film_id, genre_id) " +
+                String genreQuerySql = "insert into film_genre (film_id, genre_id) " +
                         "values (?, ?)";
 
-                jdbcTemplate.batchUpdate(GENRE_QUERY_SQL, new BatchPreparedStatementSetter() {
+                jdbcTemplate.batchUpdate(genreQuerySql, new BatchPreparedStatementSetter() {
                     @Override
                     public void setValues(PreparedStatement ps, int i) throws SQLException {
                         ps.setString(1, String.valueOf(film.getId()));
@@ -63,17 +62,16 @@ public class FilmDbStorage implements FilmStorage {
         }
     }
 
-    @SuppressWarnings("checkstyle:LocalVariableName")
     @Override
     public Film getById(long id) {
         Film film = null;
-        String QUERY_SQL = "select * " +
+        String querySql = "select * " +
                 "from films as f " +
                 "join mpa as m on f.rating_id = m.rating_id " +
                 "left join film_genre as fg on f.film_id = fg.film_id " +
                 "left join genre as g on fg.genre_id = g.genre_id " +
                 "where f.film_id = ?";
-        SqlRowSet rowSet = jdbcTemplate.queryForRowSet(QUERY_SQL, id);
+        SqlRowSet rowSet = jdbcTemplate.queryForRowSet(querySql, id);
         while (rowSet.next()) {
             Genre genre = new Genre(rowSet.getInt("genre_id"),
                     rowSet.getString("genre_name"));
@@ -94,16 +92,16 @@ public class FilmDbStorage implements FilmStorage {
         return film;
     }
 
-    @SuppressWarnings("checkstyle:LocalVariableName")
+
     @Override
     public List<Film> findAll() {
         Map<Long, ArrayList<Genre>> filmGenres = getGenresMap();
 
         List<Film> filmList = new ArrayList<>();
-        String QUERY_SQL = "select * " +
+        String querySql = "select * " +
                 "from films as f " +
                 "join mpa as m on f.rating_id=m.rating_id ";
-        SqlRowSet rowSet = jdbcTemplate.queryForRowSet(QUERY_SQL);
+        SqlRowSet rowSet = jdbcTemplate.queryForRowSet(querySql);
         while (rowSet.next()) {
             Mpa mpa = new Mpa(rowSet.getInt("rating_id"),
                     rowSet.getString("mpa_name"));
@@ -122,10 +120,10 @@ public class FilmDbStorage implements FilmStorage {
         return filmList;
     }
 
-    @SuppressWarnings("checkstyle:LocalVariableName")
+
     @Override
     public Film update(Film film) {
-        String QUERY_SQL = "update films set " +
+        String querySql = "update films set " +
                 "name = ?, description = ?, release_date = ?, duration = ?, rating_id = ? " +
                 "where film_id = ?";
 
@@ -133,7 +131,7 @@ public class FilmDbStorage implements FilmStorage {
 
         int mpaId = film.getMpa().getId();
 
-        jdbcTemplate.update(QUERY_SQL,
+        jdbcTemplate.update(querySql,
                 film.getName(),
                 film.getDescription(),
                 film.getReleaseDate(),
@@ -141,14 +139,14 @@ public class FilmDbStorage implements FilmStorage {
                 mpaId,
                 film.getId());
 
-        String GENRE_REMOVE_QUERY_SGL = "delete from film_genre where film_id = ?";
-        jdbcTemplate.update(GENRE_REMOVE_QUERY_SGL, film.getId());
+        String genreRemoveQuerySql = "delete from film_genre where film_id = ?";
+        jdbcTemplate.update(genreRemoveQuerySql, film.getId());
 
         if (film.getGenres().size() > 0) {
-            String GENRE_QUERY_SQL = "insert into film_genre (film_id, genre_id) " +
+            String genreQuerySql = "insert into film_genre (film_id, genre_id) " +
                     "values (?, ?)";
 
-            jdbcTemplate.batchUpdate(GENRE_QUERY_SQL, new BatchPreparedStatementSetter() {
+            jdbcTemplate.batchUpdate(genreQuerySql, new BatchPreparedStatementSetter() {
                 @Override
                 public void setValues(PreparedStatement ps, int i) throws SQLException {
                     ps.setString(1, String.valueOf(film.getId()));
@@ -165,26 +163,26 @@ public class FilmDbStorage implements FilmStorage {
         return film;
     }
 
-    @SuppressWarnings("checkstyle:LocalVariableName")
+
     @Override
     public void deleteFilm(long id) {
-        String QUERY_SQL = "delete from films where film_id = ?";
-        jdbcTemplate.update(QUERY_SQL, id);
+        String querySql = "delete from films where film_id = ?";
+        jdbcTemplate.update(querySql, id);
     }
 
-    @SuppressWarnings("checkstyle:LocalVariableName")
+
     @Override
     public List<Film> getPopular(int count) {
         Map<Long, ArrayList<Genre>> filmGenres = getGenresMap();
 
-        String QUERY_SQL = "select f.*, count(fl.user_id) AS likes, mpa.MPA_NAME " +
+        String querySql = "select f.*, count(fl.user_id) AS likes, mpa.MPA_NAME " +
                 "from films as f " +
                 "left join film_likes as fl on f.film_id=fl.film_id " +
                 "left join mpa as mpa on f.rating_id=mpa.rating_id " +
                 "group by f.film_id " +
                 "order by likes desc  " +
                 "limit ?";
-        SqlRowSet rowSet = jdbcTemplate.queryForRowSet(QUERY_SQL, count);
+        SqlRowSet rowSet = jdbcTemplate.queryForRowSet(querySql, count);
         List<Film> filmRating = new ArrayList<>();
         while (rowSet.next()) {
             long filmId = rowSet.getLong("film_id");
@@ -223,13 +221,12 @@ public class FilmDbStorage implements FilmStorage {
         };
     }
 
-    @SuppressWarnings("checkstyle:LocalVariableName")
     private Map<Long, ArrayList<Genre>> getGenresMap() {
         Map<Long, ArrayList<Genre>> filmGenres = new HashMap<>();
-        String GENRES_QUERY_SQL = "select * " +
+        String genresQuerySql = "select * " +
                 "from film_genre as fg " +
                 "join genre as g on fg.genre_id = g.genre_id ";
-        SqlRowSet genresRowSet = jdbcTemplate.queryForRowSet(GENRES_QUERY_SQL);
+        SqlRowSet genresRowSet = jdbcTemplate.queryForRowSet(genresQuerySql);
         while (genresRowSet.next()) {
             long filmId = genresRowSet.getLong("film_id");
             Genre genre = new Genre(genresRowSet.getInt("genre_id"),
